cfe9666f7ceb4bf2fcaad03924114eb22e1eb282
Andriy Ermolenko <andriy.ermolenko@gmail.com>
2015-04-01 10:31:42 +0300

    changed styles

--- test/main.js
+++ test/main.js
@@ -5,8 +5,8 @@ var easyreport = require('../lib');
 var report = easyreport.init({
     title: 'Report1',
     desc: 'Report for testing',
-    names: ['List1', 'List2'],
-    records: [[1, 2, 3], [2, 3, 4]]
+    names: ['List1', 'List2', 'OFFFFFCOURSE', 'Test4444', 'List1', 'List2', 'OFFFFFCOURSE', 'Test4444'],
+    records: [[1, 2, 3], [2, 3, 4], [3, 2, 3], [12, 45, 5], [1, 2, 3], [2, 3, 4], [3, 2, 3], [12, 45, 5]]
 });
-var file = report.write('../');
-console.log(report.generateHTML());
+var file = report.write();
+//console.log(report.generateHTML());
