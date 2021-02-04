2a6640f342fe2fd0c9cb56456ceb13ef15df31d8
Andrii Yermolenko <ext-andrii.yermolenko@here.com>
2021-02-04 15:57:03 +0200

    First iteration on es6 migration

--- ext/docx.js
+++ ext/docx.js
@@ -1,13 +1,15 @@
-var fs = require('fs');
+const fs = require('fs');
 
-var docx = require('html-docx-js');
+const docx = require('html-docx-js');
 
 module.exports = {
-    types: ['docx'],
-    generate: function(html, options, fileName) {
-        var docxObj = docx.asBlob(html);
-        fs.writeFile(fileName, docxObj, function(err) {
-            if (err) throw err;
-        });
-    }
+  types: ['docx'],
+  generate: function(html, options, fileName) {
+    const docxObj = docx.asBlob(html);
+    fs.writeFile(fileName, docxObj, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }
 };
